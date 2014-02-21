0c6d4d2d8deb391837522a8a3791e5bc4b079f6f
lemonslice96 <lemonslice96@gmail.com>
2014-02-21 09:38:51 -0500

    cool
    cool

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int area, length = 15, width = 25;
+            area = length * width;
+            Console.WriteLine("the floor space is {0} square feet.", area);
+        }
+    }
+}
